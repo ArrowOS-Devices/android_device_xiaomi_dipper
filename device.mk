1da65ead10e062bf2dcae7f23d8d3a4d2f438bae
kubersharma001 <kubersharma001@gmail.com>
2018-10-21 19:24:10 +0530

    dipper: build qti-telephony-common from source
     * the one we were using from Oreo caused issues for Single Sim Potter users so lets use the one build from source as it works fine
    
    Change-Id: Iab472cb28950a444c1f9dd3b8d9f015a1dd7239e

--- device.mk
+++ device.mk
@@ -45,6 +45,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
 
+# Telephony
+PRODUCT_PACKAGES += \
+    qti-telephony-common
+
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
