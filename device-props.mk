59c41dd45ec7b5f443a977f1faff861c50aee7d0
foric27 <forc27101992@gmail.com>
2019-01-07 19:06:34 +0300

    dipper: adb on

--- device-props.mk
+++ device-props.mk
@@ -58,3 +58,12 @@ persist.camera.sat.fallback.dist=45 \
 persist.camera.sat.fallback.dist.d=5 \
 persist.camera.sat.fallback.luxindex=310 \
 persist.camera.sat.fallback.lux.d=50
+
+# Set default log size on userdebug/eng build to 16M
+PRODUCT_PROPERTY_OVERRIDES += ro.logd.size=16M
+
+# prop,default overides
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.adb.secure=0 \
+    persist.sys.usb.config=adb 
+
